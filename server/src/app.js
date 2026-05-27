7475a22b6fc9b9fe1970a6c9b19aa0d92d3c3c21
jmcook_21 <jmcook5520@gmail.com>
2026-05-26 20:13:21 -0400

    Basic test for calling Claude API

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,9 @@ const app = express();
 app.use(express.json());
 
 const healthRoute = require('./routes/health');
+const estimateRoute = require('./routes/estimate');
+
 app.use('/health', healthRoute);
+app.use('/estimate', estimateRoute);
 
 module.exports = app;
